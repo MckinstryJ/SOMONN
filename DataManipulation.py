451afc0cff2bfab1a79ef3ee0bd3aea1339d5b4e
John Mckinstry <xzten3@gmail.com>
2018-04-25 10:54:22 -0600

    finished project

---
+++ DataManipulation.py
@@ -0,0 +1,76 @@
+import os
+
+date = []
+openData = []
+highData = []
+lowData = []
+closeData = []
+adjCloseData = []
+volumeData = []
+percent_openData = []
+percent_highData = []
+percent_lowData = []
+percent_closeData = []
+percent_adjCloseData = []
+percent_volumeData = []
+
+def fill(tix):
+    file = open(tix + ".csv", "r")
+
+    for line in file:
+        seperatedData = line.split(",")
+        date.append(seperatedData[0])
+        openData.append(float(seperatedData[1]))
+        highData.append(float(seperatedData[2]))
+        lowData.append(float(seperatedData[3]))
+        closeData.append(float(seperatedData[4]))
+        adjCloseData.append(float(seperatedData[5]))
+        volumeData.append(int(seperatedData[6]))
+
+    adjust()
+
+def adjust():
+    for x in range(1,len(closeData)):
+        percent_openData.append((openData[x]/openData[x-1]) - 1)
+        percent_highData.append((highData[x]/highData[x-1]) - 1)
+        percent_lowData.append((lowData[x]/lowData[x-1]) - 1)
+        percent_closeData.append((closeData[x]/closeData[x-1]) - 1)
+        percent_adjCloseData.append((adjCloseData[x]/closeData[x-1]) - 1)
+        percent_volumeData.append((volumeData[x]/volumeData[x-1]) - 1)
+
+def trainingInputData():
+    fullSet = []
+    for x in range(0, len(percent_closeData)-1):
+        fullSet.append([percent_openData[x], percent_highData[x], percent_lowData[x],
+                        percent_closeData[x], percent_adjCloseData[x], percent_volumeData[x]])
+    return fullSet
+
+def trainingOutputData():
+    fullSet = []
+
+    for i in range(1,len(percent_closeData)):
+        fullSet.append(percent_closeData[i])
+
+    return fullSet
+
+def get_current(config, trait):
+    with open(config, 'r') as f:
+        for ln in f:
+            if ln.startswith(trait):
+                return ln
+
+def changeConfig(config, old, new, ID):
+    text = ""
+    with open(config, 'r') as base_file:
+        text = base_file.readlines()
+        base_file.close()
+
+    write_path = 'config_{!r}'.format(ID)
+
+    new_config = open(write_path, 'w')
+
+    for line in text:
+        if old in line:
+            line = line.replace(old, new)
+        new_config.write(line)
+    new_config.close()
---
+++ neat/parallel.py
@@ -0,0 +1,32 @@
+"""
+Runs evaluation functions in parallel subprocesses
+in order to evaluate multiple genomes at once.
+"""
+from multiprocessing import Pool
+
+class ParallelEvaluator(object):
+    def __init__(self, num_workers, eval_function, inputs, outputs, timeout=None):
+        """
+        eval_function should take one argument, a tuple of
+        (genome object, config object), and return
+        a single float (the genome's fitness).
+        """
+        self.num_workers = num_workers
+        self.eval_function = eval_function
+        self.timeout = timeout
+        self.pool = Pool(num_workers)
+        self.inputs = inputs
+        self.outputs = outputs
+
+    def __del__(self):
+        self.pool.close() # should this be terminate?
+        self.pool.join()
+
+    def evaluate(self, genomes, config):
+        jobs = []
+        for ignored_genome_id, genome in genomes:
+            jobs.append(self.pool.apply_async(self.eval_function, (genome, config, self.inputs, self.outputs)))
+
+        # assign the fitness back to each genome
+        for job, (ignored_genome_id, genome) in zip(jobs, genomes):
+            genome.fitness = job.get(timeout=self.timeout)
